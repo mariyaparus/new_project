36c86ad13e83c4cae242d3228d42ecbd4df4c3cf
MariyaParus <cat-she@mail.ru>
2023-05-12 12:07:13 +0300

    add capitalize_words to str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def uppercase_string(input_string):
+    """
+    Функция делает все буквы заглавными
+    """
     return input_string.upper()
+
+
+def capitalize_words(input_string):
+    """
+    Функция делает первые буквы заглавными
+    """
+    return input_string.title()
